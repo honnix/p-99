db26c4cf69d4fde9652b85bd27d2c12ea538e6a8
ehonlia <hongxin.liang@ericsson.com>
2016-09-21 11:46:48 +0200

    enough for now

--- src/main/scala/my_lists.scala
+++ src/main/scala/my_lists.scala
@@ -16,10 +16,8 @@ object MyList {
     def elem0[T](list: List[T], cur: Int, till: Int): T =
       if (cur == till)
         list.head
-      else {
-        val next = cur + 1
-        elem0(list.tail, next, till)
-      }
+      else
+        elem0(list.tail, cur + 1, till)
 
     elem0(list, 0, index)
   }
@@ -54,11 +52,11 @@ object MyList {
     reverse(append0(reverse(list1), list2))
   }
 
-  def flatten[T](list: List[T]): List[T] = list match {
+  def flatten[T](list: List[_]): List[T] = list match {
     case Nil => Nil
     case head :: tail => head match {
-      case l: List[T] @unchecked => append(flatten(l), flatten(tail))
-      case _ => head :: flatten(tail)
+      case l: List[_] => append(flatten(l), flatten(tail))
+      case _ => head.asInstanceOf[T] :: flatten(tail)
     }
   }
 
@@ -97,5 +95,54 @@ object MyList {
       case x :: Nil => x
       case x => List(length(x), x.head)
     }
+  }
+
+  // 1.12
+  def decode[T](list: List[_]): List[T] = {
+    val list1 = list.map {
+      case count :: elem :: Nil => List.fill(count.asInstanceOf[Int])(elem)
+      case x => x
+    }
+    flatten(list1)
+  }
+
+  // 1.13
+  def encode_direct[T](list: List[T]) = {
+    def encode_direct0(list1: List[T], list2: List[_], cur: T): List[_] = list1 match {
+      case Nil => list2
+      case head :: tail if head == cur => list2.head match {
+        case count :: elem :: Nil => encode_direct0(tail, List(count.asInstanceOf[Int] + 1, elem) :: list2.tail, cur)
+        case elem => encode_direct0(tail, List(2, elem) :: list2.tail, cur)
+      }
+      case _ => encode_direct0(list1.tail, list1.head :: list2, list1.head)
+    }
+
+    reverse(encode_direct0(list.tail, List(list.head), list.head))
+  }
+
+  // 1.14
+  def dupli[T](list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case head :: tail => head :: head :: dupli(tail)
+  }
+
+  // 1.15
+  def dupli[T](list: List[T], count: Int): List[T] = list match {
+    case Nil => Nil
+    case head :: tail => append(List.fill(count)(head), dupli(tail, count))
+  }
+
+  // 1.16
+  def drop[T](list: List[T], count: Int) = {
+    def drop0[T](list: List[T], cur: Int, count: Int): List[T] = list match {
+      case Nil => Nil
+      case head :: tail =>
+        if (cur == count)
+        drop0(tail, 1, count)
+      else
+        head :: drop0(tail, cur + 1, count)
+    }
+
+    drop0(list, 1, count)
   }
 }
