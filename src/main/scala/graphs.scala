a3dace026bfdaf368e1bd7af73cef73a3e4fb6e9
ehonlia <hongxin.liang@ericsson.com>
2016-10-22 17:33:26 +0200

    6.05

--- src/main/scala/graphs.scala
+++ src/main/scala/graphs.scala
@@ -1,6 +1,7 @@
 package p99
 
 case class Graph[T](nodes: List[T], edges: List[(T, T)])
+case class WGraph[T](nodes: List[T], edges: List[(T, T, Int)])
 
 object Graphs {
   val graph1 = Graph(
@@ -12,6 +13,12 @@ object Graphs {
     List('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'),
     List(('a', 'b'), ('a', 'd'), ('b', 'c'), ('b', 'e'), ('c', 'e'),
       ('e', 'h'), ('d', 'e'), ('d', 'f'), ('d', 'g'), ('g', 'h'))
+  )
+
+  val graph3 = WGraph(
+    List('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'),
+    List(('a', 'b', 5), ('a', 'd', 3), ('b', 'c', 2), ('b', 'e', 4), ('c', 'e', 6),
+      ('e', 'h', 5), ('d', 'e', 7), ('d', 'f', 4), ('d', 'g', 3), ('g', 'h', 1))
   )
 
   def neighbours[T](graph: Graph[T], node: T) = {
@@ -42,21 +49,44 @@ object Graphs {
       .map(_ ::: List(from))
 
   // 6.04
-  def spanningTree(graph: Graph[Char]) = {
-    def select_edges(nodes: List[Char], edges: Stream[(Char, Char)]): Stream[List[(Char, Char)]] = {
-      def accept(nodes: List[Char], edge: (Char, Char)) = {
-        nodes.contains(edge._1) && !nodes.contains(edge._2) ||
-        nodes.contains(edge._2) && !nodes.contains(edge._1)
-      }
-
-      if (nodes == Nil) Stream(Nil)
-      else
-        edges.filter(accept(nodes, _)).flatMap { x =>
-          val nodes1 = nodes.filterNot(y => y == x._1 || y == x._2)
-          val edges1 = edges.filterNot(_ == x)
-          select_edges(nodes1, edges1).map(x :: _)
-      }
+  def select_edges[T](nodes: List[T], edges: Stream[(T, T, Int)]): Stream[List[(T, T, Int)]] = {
+    def accept(nodes: List[T], edge: (T, T, Int)) = {
+      nodes.contains(edge._1) && !nodes.contains(edge._2) ||
+      nodes.contains(edge._2) && !nodes.contains(edge._1)
     }
-    select_edges(graph.nodes.tail, graph.edges.toStream).map(x => Graph(graph.nodes, x.sorted)).distinct
+
+    println(edges.toList)
+    if (nodes == Nil) Stream(Nil)
+    else
+      edges.filter(accept(nodes, _)).flatMap { x =>
+        val nodes1 = nodes.filterNot(y => y == x._1 || y == x._2)
+        val edges1 = edges.filterNot(_ == x)
+        select_edges(nodes1, edges1).map(x :: _)
+      }
+  }
+
+  def spanningTree[T](graph: WGraph[T]) = {
+    select_edges(graph.nodes.tail, graph.edges.toStream).map { x =>
+      WGraph(graph.nodes, x.sortWith { (a, b) =>
+        val s1 = a._1.toString
+        val s2 = a._2.toString
+        val s3 = b._1.toString
+        val s4 = b._2.toString
+        s1 < s3 || s1 == s3 && s2 < s4
+      })
+    }.distinct    
+  }
+
+  // 6.05
+  def minimalSpanningTree[T](graph: WGraph[T]) = {
+    select_edges(graph.nodes.tail, graph.edges.sortBy(_._3).toStream).map { x =>
+      WGraph(graph.nodes, x.sortWith { (a, b) =>
+        val s1 = a._1.toString
+        val s2 = a._2.toString
+        val s3 = b._1.toString
+        val s4 = b._2.toString
+        s1 < s3 || s1 == s3 && s2 < s4
+      })
+    }.head.edges.foldLeft(0)(_ + _._3)
   }
 }
